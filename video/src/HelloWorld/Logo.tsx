a3f3109a5fba7541251de1685a4cc06697fe50c0
Kwesi Johnson <kwesijohnsonfuturecreatives@gmail.com>
2026-08-20 12:11:15 +0000

    Use SVG F logo instead of image import

--- video/src/HelloWorld/Logo.tsx
+++ video/src/HelloWorld/Logo.tsx
@@ -1,17 +1,13 @@
 import { zColor } from "@remotion/zod-types";
 import { AbsoluteFill } from "remotion";
 import { z } from "zod";
-import logoImage from "./future-creatives-logo.png";
 
 export const myCompSchema2 = z.object({
   logoColor1: zColor(),
   logoColor2: zColor(),
 });
 
-export const Logo: React.FC<z.infer<typeof myCompSchema2>> = ({
-  logoColor1: color1,
-  logoColor2: color2,
-}) => {
+export const Logo: React.FC<z.infer<typeof myCompSchema2>> = () => {
   return (
     <AbsoluteFill
       style={{
@@ -19,15 +15,14 @@ export const Logo: React.FC<z.infer<typeof myCompSchema2>> = ({
         alignItems: "center",
       }}
     >
-      <img
-        src={logoImage}
-        style={{
-          width: "400px",
-          height: "400px",
-          objectFit: "contain",
-        }}
-        alt="Future Creatives"
-      />
+      <svg viewBox="0 0 240 280" width="300" height="350">
+        {/* Left vertical bar */}
+        <rect x="50" y="30" width="40" height="200" rx="20" fill="#00D9D9" stroke="#000000" strokeWidth="6" />
+        {/* Top horizontal bar */}
+        <rect x="50" y="30" width="130" height="40" rx="20" fill="#00D9D9" stroke="#000000" strokeWidth="6" />
+        {/* Middle horizontal bar */}
+        <rect x="50" y="110" width="110" height="40" rx="20" fill="#00D9D9" stroke="#000000" strokeWidth="6" />
+      </svg>
     </AbsoluteFill>
   );
 };
